f6d3df822e85f1c2c7bdc5509206349b8d5f309b
Sharon <moonthin@hotmail.com>
2021-11-23 15:23:25 +0800

    add filtering

--- read.py
+++ read.py
@@ -13,8 +13,17 @@ with open('reviews.txt', 'r') as f:
 print('檔案讀取完畢, 總共有', len(data), '筆資料')
 
 sum_len = 0
-for d in data:  #data裡每一筆資料叫d
+for d in data:  #data裡每一筆資料(留言 )叫d
 	sum_len = sum_len +len(d) # sum_len += len(d)
 	# print(sum_len)
 
 print('留言的平均長度是', sum_len/len(data))
+
+new = []   #篩選
+for d in data: #d是每一筆留言(字串) data就是裝所有d 的(清單)
+	if len(d) < 100:
+		new.append(d) #小於100的留言增加在new[]裡面.
+print('一共有', len(new), '筆留言長度小於100')  #print對齊new
+                             #等loop for 都跑完 print才印一次
+print(new[0])
+print(new[1])
